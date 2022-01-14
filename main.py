b3b642d5426ba9b14673b689c0b87c4a2c90459d
Justin <moeh9589@bears.unco.edu>
2022-01-13 22:50:09 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+cost = 6120.93
+ticker_dict = {"TWLO": 5, "PAYC": 4, "APPN": 20, "SHOP": 2, "NVDA": 2, "NOW": 2}
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+
+total_amt = 0
+
+for ticker in ticker_dict:
+    ticker_str = f"https://finance.yahoo.com/quote/{ticker}/history?p={ticker}"
+    print("")
+    driver.get(ticker_str)
+    time.sleep(1)
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    time.sleep(1.2)
+    item = soup.find('fin-streamer', attrs={"data-reactid": "47"})
+    # print(item.text)
+    newnum = str(item.text)
+    stock_amt = float(newnum.replace(',', ''))*ticker_dict[ticker]
+    total_amt = total_amt + stock_amt
+    print(f"{ticker} : {item} : {stock_amt:.2f}")
+
+driver.quit()
+
+
+profit = total_amt - cost
+print("")
+print(f"Profit = ${profit:.2f}")
+total = cost + profit
+print(f"Total = ${total:.2f}")
+print("")
